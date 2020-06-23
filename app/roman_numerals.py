c4d59fd56c9635fd2330c7aa4fab0f332d8c6e5c
Ben Perlmutter <benperlmutter94@gmail.com>
2020-06-23 09:59:17 -0500

    add error handling and remove print statements

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -8,24 +8,22 @@ def parse(roman_num):
     
     idx = len(roman_num)-1
     nums = []
+    try:    
+        while(idx >= 0):
+            if(idx == 0):
+                nums.append(roman_numerals[roman_num[idx]])
+                idx -= 1
+            elif(roman_numerals[roman_num[idx]] <= roman_numerals[roman_num[idx-1]]):
+                nums.append(roman_numerals[roman_num[idx]])
+                idx -= 1
+            elif(roman_numerals[roman_num[idx]] > roman_numerals[roman_num[idx-1]]):
+                numeral = roman_num[idx-1]+roman_num[idx]
+                nums.append(roman_numerals[numeral])
+                idx -= 2
+        return sum(nums)
 
-    while(idx >= 0):
-        print('idx', idx)
-        if(idx == 0):
-            nums.append(roman_numerals[roman_num[idx]])
-            idx -= 1
-        elif(roman_numerals[roman_num[idx]] <= roman_numerals[roman_num[idx-1]]):
-            print("in order", roman_num[idx], roman_num[idx-1])
-            nums.append(roman_numerals[roman_num[idx]])
-            idx -= 1
-        elif(roman_numerals[roman_num[idx]] > roman_numerals[roman_num[idx-1]]):
-            print("order rev", roman_num[idx], roman_num[idx-1])
-            numeral = roman_num[idx-1]+roman_num[idx]
-            nums.append(roman_numerals[numeral])
-            idx -= 2
-        print(nums)
-    
-    return sum(nums)
+    except:
+        return 'The number you passed in was not a correctly formatted Roman numeral!'
 
 
-print('final', parse('XCIX'))
+print('final', parse('XCIXQQ'))
